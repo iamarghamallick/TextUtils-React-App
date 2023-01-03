d3cd98b09d6ec87aa97a85073b27f5a2cad061da
Argha Mallick <arghamallick5@gmail.com>
2023-01-03 20:56:56 +0530

    Dark mode function added

--- src/App.js
+++ src/App.js
@@ -22,12 +22,13 @@ function App() {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success");
+      showAlert("Dark mode enabled!", "success");
+      
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
+      showAlert("Light mode enabled!", "success");
     }
   }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,28 +4,27 @@ import PropTypes from 'prop-types';
 function Navbar(props) {
     return (
         <>
-        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-            <div className="container-fluid">
-                <a className="navbar-brand" to="/">{props.title}</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+                <a className="navbar-brand" href="#">{props.title}</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-a" aria-current="page" to="/">Home</a>
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-a" to="/about">{props.aboutText}</a>
+                            <a className="nav-link" href="#">About</a>
                         </li>
                     </ul>
                     <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
-                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">DarkMode</label>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
         </>
     )
 }
